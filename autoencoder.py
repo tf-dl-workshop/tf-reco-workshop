fe83f30367986652c13d4a6a9fe99e5b11d912a5
James S <sorawitj@gmail.com>
2017-05-29 00:23:32 +0700

    small fix on data prep

--- autoencoder.py
+++ autoencoder.py
@@ -9,11 +9,11 @@ from tensorflow.python.estimator.inputs import numpy_io
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
-train_arr, test_arr = dataprep.get_train_test()
+train_arr, test_arr, train_eval_arr = dataprep.get_train_test()
 
 train_rating = {'ratings': train_arr.astype(np.float32)}
-test_eval_rating = {'ratings': train_arr.astype(np.float32), 'targets': test_arr.astype(np.float32)}
-train_eval_rating = {'ratings': train_arr.astype(np.float32), 'targets': train_arr.astype(np.float32)}
+test_eval_rating = {'ratings': train_eval_arr.astype(np.float32), 'targets': test_arr.astype(np.float32)}
+train_eval_rating = {'ratings': train_eval_arr.astype(np.float32), 'targets': train_eval_arr.astype(np.float32)}
 
 
 def auto_encoder(features, targets, mode, params):
@@ -24,26 +24,24 @@ def auto_encoder(features, targets, mode, params):
         # Use tf.shape(a_t, out_type=tf.int64) instead of a_t.get_shape() if tensor shape is dynamic
         sparse_ratings = tf.SparseTensor(idx, tf.gather_nd(ratings, idx), ratings.get_shape())
 
-    with tf.name_scope("encoder"):
-        with tf.variable_scope("encoder"):
-            encoder_w = tf.get_variable("e_w", shape=[params['n_users'], params['n_dims']])
-            encoder_b = tf.get_variable("e_b", shape=[params['n_dims']], initializer=tf.zeros_initializer)
+    with tf.variable_scope("encoder"):
+        encoder_w = tf.get_variable("e_w", shape=[params['n_users'], params['n_dims']])
+        encoder_b = tf.get_variable("e_b", shape=[params['n_dims']], initializer=tf.zeros_initializer)
 
-        encoding_tensor = tf.sigmoid(tf.sparse_tensor_dense_matmul(sparse_ratings, encoder_w)) + encoder_b
+        encoding_op = tf.sigmoid(tf.sparse_tensor_dense_matmul(sparse_ratings, encoder_w)) + encoder_b
 
-    with tf.name_scope("decoder"):
-        with tf.variable_scope("decoder"):
-            decoder_w = tf.get_variable("d_w", shape=[params['n_dims'], params['n_users']])
-            decoder_b = tf.get_variable("d_b", shape=[params['n_users']], initializer=tf.zeros_initializer)
+    with tf.variable_scope("decoder"):
+        decoder_w = tf.get_variable("d_w", shape=[params['n_dims'], params['n_users']])
+        decoder_b = tf.get_variable("d_b", shape=[params['n_users']], initializer=tf.zeros_initializer)
 
-        decoding_tensor = tf.identity(tf.matmul(encoding_tensor, decoder_w)) + decoder_b
+        decoding_op = tf.identity(tf.matmul(encoding_op, decoder_w)) + decoder_b
 
     loss = None
     if mode == model_fn_lib.ModeKeys.TRAIN:
         with tf.name_scope("loss"):
             # backpropagate only partial observed ratings
-            neg_sparse_decoding = tf.SparseTensor(idx, tf.negative(tf.gather_nd(decoding_tensor, idx)),
-                                                  decoding_tensor.get_shape())
+            neg_sparse_decoding = tf.SparseTensor(idx, tf.negative(tf.gather_nd(decoding_op, idx)),
+                                                  decoding_op.get_shape())
             reg_loss = layers.apply_regularization(layers.l2_regularizer(scale=params['l2reg']),
                                                    weights_list=[encoder_w, decoder_w])
             diff = tf.sparse_add(sparse_ratings, neg_sparse_decoding).values
@@ -54,8 +52,8 @@ def auto_encoder(features, targets, mode, params):
             target_idx = tf.where(tf.not_equal(targets, 0.0))
             # Use tf.shape(a_t, out_type=tf.int64) instead of a_t.get_shape() if tensor shape is dynamic
             sparse_targets = tf.SparseTensor(target_idx, tf.gather_nd(targets, target_idx), targets.get_shape())
-            neg_sparse_decoding = tf.SparseTensor(target_idx, tf.negative(tf.gather_nd(decoding_tensor, target_idx)),
-                                                  decoding_tensor.get_shape())
+            neg_sparse_decoding = tf.SparseTensor(target_idx, tf.negative(tf.gather_nd(decoding_op, target_idx)),
+                                                  decoding_op.get_shape())
             diff = tf.sparse_add(sparse_targets, neg_sparse_decoding).values
             loss = tf.sqrt(tf.reduce_mean(tf.square(diff)))
 
@@ -88,27 +86,27 @@ with tf.Session() as sess:
         learning_rate=0.0001
     )
 
-    #input queue for training
+    # input queue for training
     train_input_fn = numpy_io.numpy_input_fn(
         x=train_rating, y=np.zeros(shape=[train_arr.shape[0], 1]), batch_size=256, shuffle=True, num_epochs=None)
-    #input queue for evaluation on test data
+    # input queue for evaluation on test data
     test_eval_input_fn = numpy_io.numpy_input_fn(
         x=test_eval_rating, y=np.zeros(shape=[test_arr.shape[0], 1]), batch_size=test_arr.shape[0], shuffle=False,
         num_epochs=None)
-    #input queue for evaluation on training data
+    # input queue for evaluation on training data
     train_eval_input_fn = numpy_io.numpy_input_fn(
         x=train_eval_rating, y=np.zeros(shape=[test_arr.shape[0], 1]), batch_size=test_arr.shape[0], shuffle=False,
         num_epochs=None)
 
-    validation_monitor = monitors.ValidationMonitor(input_fn=test_eval_input_fn, eval_steps=1, every_n_steps=500,
+    validation_monitor = monitors.ValidationMonitor(input_fn=test_eval_input_fn, eval_steps=1, every_n_steps=100,
                                                     name='test')
-    train_monitor = monitors.ValidationMonitor(input_fn=train_eval_input_fn, eval_steps=1, every_n_steps=500,
+    train_monitor = monitors.ValidationMonitor(input_fn=train_eval_input_fn, eval_steps=1, every_n_steps=100,
                                                name='train')
 
     autoencoder_cf = Estimator(
         model_fn=auto_encoder,
         params=model_params,
-        model_dir='model/_summary/auto_rec',
+        model_dir='model/_summary/auto_rec_test',
         config=RunConfig(save_checkpoints_secs=60))
 
     autoencoder_cf.fit(input_fn=train_input_fn, steps=15000, monitors=[validation_monitor, train_monitor])
--- data/dataprep.py
+++ data/dataprep.py
@@ -9,12 +9,18 @@ def get_train_test():
 
     rating_df = pd.read_csv('data/ml-100k/u.data', sep='\t', names=cols)
 
-    train, test = train_test_split(rating_df, test_size=0.1, random_state=1234)
-
-    train_matrix = train.pivot(index='movie_id', columns='user_id', values='rating').fillna(0.0)
-    test_matrix = test.pivot(index='movie_id', columns='user_id', values='rating').fillna(0.0)
+    seed = 123
+    while True:
+        train, test = train_test_split(rating_df, test_size=0.2, random_state=seed)
+        train_matrix = train.pivot(index='movie_id', columns='user_id', values='rating').fillna(0.0)
+        test_matrix = test.pivot(index='movie_id', columns='user_id', values='rating').fillna(0.0)
+        if len(set(train_matrix.columns).difference(set(test_matrix.columns))) > 0:
+            seed += 1
+        else:
+            break
 
     test_matrix = test_matrix.filter(items=train_matrix.index, axis=0)
-    test_matrix = test_matrix.filter(items=train_matrix.columns, axis=1)
 
-    return train_matrix.as_matrix(), test_matrix.as_matrix()
+    train_eval_matrix = train_matrix.loc[test_matrix.index, test_matrix.columns]
+
+    return train_matrix.as_matrix(), test_matrix.as_matrix(), train_eval_matrix.as_matrix()
